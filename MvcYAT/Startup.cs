16a4e66809307e06ffd6bdc972b4ef2154016972
ismailrr <ismailrajira@gmail.com>
2018-12-06 17:46:50 +0700

    Add project files.

---
+++ MvcYAT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcYAT.Startup))]
+namespace MvcYAT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
